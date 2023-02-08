cc9c72e74fc77d66eb3284ef25f38598eeb73614
meowz1 <114435142+meowz1@users.noreply.github.com>
2023-02-08 22:25:51 +0300

    Add files via upload

---
+++ snake/snake/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Data;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Threading;
+
+namespace snikers
+{
+    class snikers
+    {
+        int Height = 20;
+        int Width = 30;
+
+        int[] X = new int[50];
+        int[] Y = new int[50];
+
+        int nyamX;
+        int nyamY;
+
+        int lvls = 3;
+
+        ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
+        char key = 'w';
+        Random random = new Random();
+        private object rnd;
+
+        snikers()
+        {
+            X[0] = 5;
+            Y[0] = 5;
+            Console.CursorVisible = false;
+            nyamX = random.Next(2, (Width - 2));
+            nyamY = random.Next(2, (Height - 2));
+        }
+
+        public void Writeboard()
+        {
+            Console.Clear();
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("1");
+            }
+            for (int i = 1; i <= (Width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (Height + 2));
+                Console.Write("1");
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("1");
+
+            }
+            for (int i = 1; i <= (Height + 1); i++)
+            {
+                Console.SetCursorPosition((Width + 2), i);
+                Console.Write("1");
+            }
+        }
+        public void Input()
+        {
+            if (Console.KeyAvailable)
+            {
+                keyInfo = Console.ReadKey(true);
+                key = keyInfo.KeyChar;
+            }
+
+
+
+        }
+
+        public void zest()
+        {
+            if (X[0] == nyamX)
+            {
+                if (Y[0] == nyamY)
+                {
+                    lvls++;
+                    nyamX = random.Next(2, (Width - 2));
+                    nyamY = random.Next(2, (Height - 2));
+                }
+            }
+            for (int i = lvls; i > 1; i--)
+            {
+                X[i - 1] = X[i - 2];
+                Y[i - 1] = Y[i - 2];
+            }
+            switch (key)
+            {
+                case 'w':
+                    Y[0]--;
+                    break;
+                case 's':
+                    Y[0]++;
+                    break;
+                case 'd':
+                    X[0]++;
+                    break;
+                case 'a':
+                    X[0]--;
+                    break;
+
+            }
+            for (int i = 0; i <= (lvls - 1); i++)
+            {
+                WritePoint(X[i], Y[i]);
+                WritePoint(nyamX, nyamY);
+            }
+            Thread.Sleep(100);
+        }
+        public void WritePoint(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("0");
+        }
+        static void Main(string[] args)
+        {
+            snikers snikers = new snikers();
+            while (true)
+            {
+                snikers.Writeboard();
+                snikers.Input();
+                snikers.zest();
+            }
+            Console.ReadKey();
+        }
+    }
+}
